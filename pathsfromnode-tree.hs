c08899f66ceab3534d27ec536769f1d4d3b7691b
Arjun Khandkar <khandkararjun@gmail.com>
2019-01-04 22:40:06 +0530

    fixed pathsfromnode-tree.hs

--- pathsfromnode-tree.hs
+++ pathsfromnode-tree.hs
@@ -6,7 +6,6 @@ data Tree a = Node a [Tree a]
 pathsFromNode :: Eq a => a -> [(a,a)] -> Tree a
 pathsFromNode start graph = Node start nextNodes
   where
-    nextNodes = map (\(_,s) -> pathsFromNode s (filter (\(f,s) -> f/=start&&s/=start) graph))
+    nextNodes = map (\(_,s) -> pathsFromNode s (filter (\(f,s) -> f/=start && s/=start) graph))
                 (filter (\(f,_) -> f==start) (map (\(f,s) -> if | s==start  -> (s,f) | otherwise -> (f,s)) graph))
-
 
