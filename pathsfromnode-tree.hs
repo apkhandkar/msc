69538960dc7cdfd32d5e29322a8455e9c7de1bda
Arjun Khandkar <khandkararjun@gmail.com>
2019-01-04 22:39:16 +0530

    sundry

--- pathsfromnode-tree.hs
+++ pathsfromnode-tree.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE MultiWayIf #-}
+
 data Tree a = Node a [Tree a]
             deriving (Eq,Ord,Show,Read)
 
 pathsFromNode :: Eq a => a -> [(a,a)] -> Tree a
 pathsFromNode start graph = Node start nextNodes
   where
-    nextNodes = map (\(_,s) -> pathsFromNode s graph) (filter (\(f,_) -> f==start) graph)
+    nextNodes = map (\(_,s) -> pathsFromNode s (filter (\(f,s) -> f/=start&&s/=start) graph))
+                (filter (\(f,_) -> f==start) (map (\(f,s) -> if | s==start  -> (s,f) | otherwise -> (f,s)) graph))
+
+
