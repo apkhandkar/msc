c3289e6dab6cc8daaa60f0d50c8728af7217a494
Arjun Khandkar <khandkararjun@gmail.com>
2019-02-22 20:40:02 +0530

    add definition of selection

--- algo/selection.hs
+++ algo/selection.hs
@@ -5,4 +5,15 @@ smallest (e:[]) = e
 smallest (e:es) | e < (smallest es) = e
                 | otherwise         = smallest es
 
--- use smallest to sort a list
+-- remove the first occurence of an element from a 
+-- list
+remove :: Eq a => a -> [a] -> [a]
+remove _ [] = []
+remove e (f:fs) | e == f    = fs
+                | otherwise = (f:(remove e fs)) 
+
+-- use smallest & remove to sort a list
+selection :: Ord a => [a] -> [a]
+selection [] = []
+selection es = (f:(selection (remove f es)))
+    where f = (smallest es) 
