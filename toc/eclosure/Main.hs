77fbfc33eaa5fe7cf3642578e7b254b16f3c9846
Arjun Khandkar <khandkararjun@gmail.com>
2019-03-10 17:00:58 +0530

    code shortening

--- toc/eclosure/Main.hs
+++ toc/eclosure/Main.hs
@@ -5,18 +5,17 @@ import Helpers
 import EClosure
 
 main :: IO ()
-main =
+main = 
     fmap lines getContents >>=
         \contents ->
-            let rawInputs = tokenise ';' (head contents)
-                rawTransitions = map (\(x,y) -> (x, map (tokenise ',') y))
-                                    (map (\(x,y) -> (x, tokenise ';' y))
-                                        (map (\x -> (head x, concat $ tail x))
-                                            (map (tokenise ':') (tail contents))))
-                alphabet = getAlphabet rawInputs
-                transitions = concat $ map (buildTransitionsFor alphabet 0) rawTransitions
+            let alphabet = getAlphabet $ tokenise ';' (head contents)
+                transitions = concat $ map (buildTransitionsFor alphabet 0)
+                    (map (\(x,y) -> (x, map (tokenise ',') y))
+                        (map (\(x,y) -> (x, tokenise ';' y))
+                            (map (\x -> (head x, concat $ tail x))
+                                (map (tokenise ':') (tail contents)))))
                 states = inferStates transitions
-                closures = zip states (map (epsClosureT transitions) states)
+                closures = zip states $ map (epsClosureT transitions) states
 
             in  printClosures closures >>
-                return ()
+                return()
