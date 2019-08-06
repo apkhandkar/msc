dcb0e045d1b64d1f4dad21cb2eaf4516ace4c1d9
Arjun Khandkar <khandkararjun@gmail.com>
2019-08-06 22:22:42 +0530

    added comments to Main.hs

--- toc/eclosure/Main.hs
+++ toc/eclosure/Main.hs
@@ -17,4 +17,11 @@ main =
                 states = inferStates transitions
                 closures = zip states $ map (epsClosureT transitions) states
 
-            in  (>>) (printClosures closures) (return ()))
+            in  printClosures closures >> 
+                return ())
+
+--  How main is structured:
+--  (>>=) :: Monad m => m a -> (a -> m b) -> m b$, where:
+--  m a = (fmap lines getContents) :: IO [String] and
+--  (a -> m b) = (\contents -> ... in ... return ()) :: [String] -> IO () and thus
+--  m b = main :: IO (), Q.E.D.
