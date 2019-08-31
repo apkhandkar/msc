93a0e962d8d428b105c19edaece6b69b7d118b96
Arjun Khandkar <khandkararjun@gmail.com>
2019-08-31 11:23:21 +0530

    Minor modifications: map f (map g x) == map (f . g) x

--- toc/eclosure/Main.hs
+++ toc/eclosure/Main.hs
@@ -10,10 +10,13 @@ main =
         (\contents ->
             let alphabet = getAlphabet $ tokenise ';' (head contents)
                 transitions = concat $ map (buildTransitionsFor alphabet 0)
-                    (map (\(x,y) -> (x, map (tokenise ',') y))
-                        (map (\(x,y) -> (x, tokenise ';' y))
-                            (map (\x -> (head x, concat $ tail x))
-                                (map (tokenise ':') (tail contents)))))
+-- [0]
+                    (map ((\(x,y) -> (x, map (tokenise ',') y)) . (\(x,y) -> (x, tokenise ';' y)) . (\x -> (head x, concat $ tail x)) . (tokenise ':')) (tail contents))
+-- [0] previously represented as:
+--                    (map (\(x,y) -> (x, map (tokenise ',') y))
+--                        (map (\(x,y) -> (x, tokenise ';' y))
+--                            (map (\x -> (head x, concat $ tail x))
+--                                (map (tokenise ':') (tail contents)))))
                 states = inferStates transitions
                 closures = zip states $ map (epsClosureT transitions) states
 
