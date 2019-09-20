d00c53b03d6fdcd941580874bf74bf1244e24d11
Arjun Khandkar <khandkararjun@gmail.com>
2019-09-20 22:04:37 +0530

    removed annotations

--- toc/eclosure/Main.hs
+++ toc/eclosure/Main.hs
@@ -9,21 +9,9 @@ main =
         (\contents ->
             let alphabet = getAlphabet $ tokenise ';' (head contents)
                 transitions = concat $ map (buildTransitionsFor alphabet 0)
--- [0]
                     (map ((\(x,y) -> (x, map (tokenise ',') y)) . (\(x,y) -> (x, tokenise ';' y)) . (\x -> (head x, concat $ tail x)) . (tokenise ':')) (tail contents))
                 states = inferStates transitions
                 closures = zip states $ map (epsClosureT transitions) states
 
             in  printClosures closures >> 
                 return ())
-
--- [0] previously represented as:
---                    (map (\(x,y) -> (x, map (tokenise ',') y))
---                        (map (\(x,y) -> (x, tokenise ';' y))
---                            (map (\x -> (head x, concat $ tail x))
---                                (map (tokenise ':') (tail contents)))))
-
---  (>>=) :: Monad m => m a -> (a -> m b) -> m b, where:
---  m a = (fmap lines getContents) :: IO [String] and
---  (a -> m b) = (\contents -> ... in ... return ()) :: [String] -> IO () and thus
---  m b = main :: IO ()
