0575a1459f3933fdf027fb4bb5653a0c08352450
Arjun Khandkar <khandkararjun@gmail.com>
2020-02-02 22:10:46 +0530

    cleanup, renamed functions

--- string-editor/SELib.hs
+++ string-editor/SELib.hs
@@ -1,25 +1,17 @@
 module SELib
     ( ini,
-      s_ini,
       ins,
-      s_ins,
       ina,
-      s_ina,
       insertAt,
       insertAfter,
       deleteN,
-      s_deleteN,
       del,
-      s_del,
       deleteAt,
       moveLeft,
-      s_moveLeft,
       moveRight,
-      s_moveRight,
       cursorMark,
       s_cursorMark,
       changeCursor,
-      s_changeCursor,
       tokenise ) where
 
 import SETypes
@@ -28,12 +20,8 @@ import Control.Monad.State
 
 -- initialise (ini)
 
-ini :: String -> SEState
-ini str =
-    (SEState{string=str,cursor=(length str),marker='^'})
-
-s_ini :: String -> State SEState ()
-s_ini i =
+ini :: String -> State SEState ()
+ini i =
     state $ \_ -> ((), SEState{string=i,cursor=(length i),marker='^'})
 
 -- insert at location (ins)
@@ -46,12 +34,8 @@ insertAt n s0 [] =
 insertAt n s0 (x:xs) =
     x:(insertAt (n-1) s0 xs)
 
-ins :: SEState -> [Char] -> SEState
-ins state@(SEState{string=s,cursor=c,marker=m}) s0 = 
-    (SEState{string=(insertAt c s0 s),cursor=(c + length s0),marker=m})
-
-s_ins :: String -> State SEState ()
-s_ins s0 = 
+ins :: String -> State SEState ()
+ins s0 = 
     state $ \SEState{string=s,cursor=c,marker=m} -> ((), SEState{string=(insertAt c s0 s),cursor=(c + length s0),marker=m})
 
 -- insert after location (ina)
@@ -63,12 +47,8 @@ insertAfter n s0 s1 = pre ++ s1 ++ post
         post = snd splits0
         splits0 = splitAt n s0
 
-ina :: SEState -> [Char] -> SEState
-ina state@(SEState{string=s,cursor=c}) s0 = 
-    (SEState{string=(insertAfter c s s0),cursor=(c + length s0),marker='^'})
-
-s_ina :: String -> State SEState ()
-s_ina s0 =
+ina :: String -> State SEState ()
+ina s0 =
     state $ \SEState{string=s,cursor=c,marker=m} -> ((), SEState{string=(insertAfter c s s0),cursor=(c + length s0),marker='^'})
 
 -- delete (del)
@@ -81,46 +61,24 @@ deleteAt n [] =
 deleteAt n (x:xs) =
     (x:(deleteAt (n-1) xs))
 
-del :: SEState -> SEState
-del state@(SEState{string=s,cursor=c,marker=m})
-    | c == (length s) =
-        (SEState{string=(deleteAt c s),cursor=(c-1),marker=m})
-    | otherwise =
-        (SEState{string=(deleteAt c s),cursor=c,marker=m})
-
-s_del :: State SEState ()
-s_del = 
+del :: State SEState ()
+del = 
     get >>= \SEState{string=s,cursor=c,marker=m} ->
     if c == (length s)
         then put SEState{string=(deleteAt c s),cursor=(c-1),marker=m}
         else put SEState{string=(deleteAt c s),cursor=c,marker=m}
 
-deleteN :: Int -> SEState -> SEState
-deleteN 0 state =
-    state
-deleteN n state =
-    deleteN (n-1) (del state)
-
-s_deleteN :: Int -> State SEState ()
-s_deleteN 0 =
+deleteN :: Int -> State SEState ()
+deleteN 0 =
     state $ \s -> ((), s)
-s_deleteN n =
-    (state $ \s -> ((), snd $ runState s_del s)) >>
-    s_deleteN (n-1)
+deleteN n =
+    (state $ \s -> ((), snd $ runState del s)) >>
+    deleteN (n-1)
 
 -- move left/right (mov l/r)
 
-moveLeft :: Int -> SEState -> SEState
-moveLeft n state@(SEState{string=s,cursor=c,marker=m})
-    | n == 0 =
-        state
-    | (c - n) < 0 =
-        (SEState{string=s,cursor=1,marker=m})
-    | otherwise =
-        (SEState{string=s,cursor=(c-n),marker=m})
-
-s_moveLeft :: Int -> State SEState ()
-s_moveLeft n =
+moveLeft :: Int -> State SEState ()
+moveLeft n =
     get >>= \i@SEState{string=s,cursor=c,marker=m} ->
     if n==0
         then put i
@@ -128,16 +86,8 @@ s_moveLeft n =
             then put SEState{string=s,cursor=1,marker=m}
             else put SEState{string=s,cursor=(c-n),marker=m}
      
-
-moveRight :: Int -> SEState -> SEState
-moveRight n state@(SEState{string=s,cursor=c,marker=m})
-    | (c + n) > (length s) = 
-        (SEState{string=s,cursor=(length s),marker=m})
-    | otherwise =
-        (SEState{string=s,cursor=(c+n),marker=m})
-
-s_moveRight :: Int -> State SEState ()
-s_moveRight n =
+moveRight :: Int -> State SEState ()
+moveRight n =
     get >>= \i@SEState{string=s,cursor=c,marker=m} ->
     if (c+n) > (length s)
         then put SEState{string=s,cursor=(length s),marker=m}
@@ -145,19 +95,15 @@ s_moveRight n =
 
 -- change cursor character
 
-changeCursor :: SEState -> Char -> SEState
-changeCursor istate@(SEState{string=s,cursor=c,marker=m}) n =
-    SEState{string=s,cursor=c,marker=n}
-
-s_changeCursor :: Char -> State SEState ()
-s_changeCursor m =
+changeCursor :: Char -> State SEState ()
+changeCursor m =
     get >>= \SEState{string=s,cursor=c,marker=_} ->
     put SEState{string=s,cursor=c,marker=m}
 
 -- generate marker line
 
 cursorMark :: Int -> Char -> String
-cursorMark n c = (take (n-1) $ repeat ' ') ++ c:[]
+cursorMark c m = (take (c-1) $ repeat ' ') ++ m:[]
 
 s_cursorMark :: State SEState String
 s_cursorMark =
